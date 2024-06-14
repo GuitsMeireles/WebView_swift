aee1395ebd5c3595a019920a798b9cbc8c9631b7
GuitsMeireles <batistaguilherme681@gmail.com>
2024-06-14 10:37:46 -0300

    Initial Commit

---
+++ IOS_WebView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IOS_WebView
+//
+//  Created by Guilherme Meireles on 14/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IOS_WebView/IOS_WebViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_WebViewApp.swift
+//  IOS_WebView
+//
+//  Created by Guilherme Meireles on 14/06/24.
+//
+
+import SwiftUI
+
+@main
+struct IOS_WebViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
